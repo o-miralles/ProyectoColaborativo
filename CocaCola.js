f80af764a9da94072253f11105686d170d2f45ac
o-miralles <st2020395@365.stucom.com>
2024-10-01 11:24:54 +0200

    Adds CocaCola.js program

---
+++ CocaCola.js
@@ -0,0 +1,6 @@
+for (var i = 1; i < 101; i++) {
+  if (i % 3 == 0) console.log('Coca');
+  else if (i % 5 == 0) console.log('Cola');
+  else if (i % 15 == 0) console.log('CocaCola');
+  else console.log(i);
+}

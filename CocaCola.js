bc70960ab64ae8417df8b29e8147c06e524f7e65
o-miralles <st2020395@365.stucom.com>
2024-10-01 11:33:10 +0200

    Changes "CocaCola" for "FizzBuzz"

--- CocaCola.js
+++ CocaCola.js
@@ -1,6 +1,6 @@
 for (var i = 1; i < 101; i++) {
   if (i % 3 == 0) console.log('Coca');
   else if (i % 5 == 0) console.log('Cola');
-  else if (i % 15 == 0) console.log('CocaCola');
+  else if (i % 15 == 0) console.log('FizzBuzz');
   else console.log(i);
 }
